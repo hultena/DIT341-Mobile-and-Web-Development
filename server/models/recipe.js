5bad246b67687950a920ec0e71272d770d9820d8
livm <livm@student.chalmers.se>
2020-09-10 13:20:44 +0200

    Instructions stored in array for list view in front end + image included in schema, #5

--- server/models/recipe.js
+++ server/models/recipe.js
@@ -3,12 +3,13 @@ const Schema = mongoose.Schema;
 
 const recipeSchema = new Schema({
     name: { type: String, required: true },
-    instructions: { type: String, required: true },
     category: String,
     cuisine: String,
     dietaryRestriction: String,
     author: { type: Schema.Types.ObjectId, ref: 'user' },
-    ingredients: [{ type: Schema.Types.ObjectId, ref: 'ingredient' }]
+    ingredients: [{ type: Schema.Types.ObjectId, ref: 'ingredient' }],
+    image: { data: Buffer, contentType: String },
+    instructions: [{ type: String }]
 });
 
 module.exports = mongoose.model('recipe', recipeSchema);
