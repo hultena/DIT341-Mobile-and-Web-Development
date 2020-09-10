217535c956458cc2046738faebe58e6da27bb7a9
livm <livm@student.chalmers.se>
2020-09-10 13:52:41 +0200

    Enums added for data specification #5

--- server/models/recipe.js
+++ server/models/recipe.js
@@ -2,13 +2,31 @@ const mongoose = require('mongoose');
 const Schema = mongoose.Schema;
 
 const recipeSchema = new Schema({
-    name: { type: String, required: true },
-    category: String,
-    cuisine: String,
+    name: {
+        type: String,
+        required: true
+    },
+    category: {
+        type: String,
+        enum: ['Baking', 'Cooking']
+    },
+    cuisine: {
+        type: String,
+        enum: [/* TODO: What types of cuisines should we allow? */]
+    },
     dietaryRestriction: String,
-    author: { type: Schema.Types.ObjectId, ref: 'user' },
-    ingredients: [{ type: Schema.Types.ObjectId, ref: 'ingredient' }],
-    image: { data: Buffer, contentType: String },
+    author: {
+        type: Schema.Types.ObjectId,
+        ref: 'user'
+    },
+    ingredients: [{
+        type: Schema.Types.ObjectId,
+        ref: 'ingredient'
+    }],
+    image: {
+        data: Buffer,
+        contentType: String
+    },
     instructions: [{ type: String }]
 });
 
