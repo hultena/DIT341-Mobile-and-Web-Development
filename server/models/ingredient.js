2102538466f08df5dbacae0bf138583e955715bd
Ella Alklid <ella.alklid@gmail.com>
2020-09-11 00:04:36 +0200

    Add length restriction to description

--- server/models/ingredient.js
+++ server/models/ingredient.js
@@ -4,7 +4,7 @@ const Schema = mongoose.Schema;
 const ingredientSchema = new Schema({
     name:{type: String, required: true},
     foodType:{type: String},
-    description:{type: String}
+    description:{type: String, maxlength: 140}
 });
 
 module.export = mongoose.model('ingredient', ingredientSchema);
