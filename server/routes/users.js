209d9f1cd070a9e80a49fe161de953fc65f07936
Ella Alklid <ella.alklid@gmail.com>
2020-09-12 22:04:27 +0200

    Add post and delete ingredient to user-routes, #18

--- server/routes/users.js
+++ server/routes/users.js
@@ -18,9 +18,11 @@ router.route('/:userId/shoppinglists/:shoppingListId')
     .get(users.getOneUserShoppingList)
 
 router.route('/:userId/ingredients')
-    .get(ingredients.getAllUserIngredients);
+    .get(ingredients.getAllUserIngredients)
+    .post(ingredients.postOneUserIngredient);
 
 router.route('/:userId/ingredients/:ingredientId')
-    .get(ingredients.getOneUserIngredient);
+    .get(ingredients.getOneUserIngredient)
+    .delete(ingredients.deleteOneUserIngredient);
 
 module.exports = router;
