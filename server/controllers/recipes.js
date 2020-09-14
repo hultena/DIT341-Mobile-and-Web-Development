05b654aa7523bf6f87bf3e399a6ae504c4edd4ea
livm <livm@student.chalmers.se>
2020-09-14 15:03:17 +0200

    User dependent requests removed from recipe controller, #12

--- server/controllers/recipes.js
+++ server/controllers/recipes.js
@@ -1,27 +1,6 @@
 const Recipe = require('../models/recipe');
-const User = require('../models/user');
 
 module.exports = {
-
-    postRecipe: async function (req, res, next) {
-        try {
-            const user = await User.findById(req.params.userId).populate('recipes');
-
-            if (user === null) next();
-            else {
-                const recipe = new Recipe(req.body);
-                recipe.user = user._id;
-
-                await recipe.save();
-                await user.recipes.push(recipe);
-                await user.save();
-
-                res.status(201).json(recipe);
-            }
-        } catch (error) {
-            next(error);
-        }
-    },
 
     getAllRecipes: async function (req, res, next) {
         try {
@@ -45,55 +24,14 @@ module.exports = {
         }
     },
 
-    // TODO: getUserRecipe, should this be here or in the user controller?
-
     // TODO: get all recipes by most likes, returning 5 at a time (?)
 
     // TODO: get all recipes filtered by newest, returning 5 at a time (?)
-
-    /*
-    * In case we come up with changes in the fields
-    * of all existing recipes, we can use below.
-    * Don't know if it's needed though?
-    */
-    updateAllRecipes: async function (req, res, next) {
-        try {
-            const recipes = await Recipe.updateMany({});
-
-            res.status(200).json(recipes);
-
-        } catch (error) {
-            next(error);
-        }
-    },
-
-    updateRecipe: async function (req, res, next) {
-        try {
-            const recipe = await Recipe.findByIdAndUpdate(req.params.recipeId, req.body);
-
-            res.status(200).json(recipe);
-
-        } catch (error) {
-            next(error);
-        }
-    },
 
     deleteAllRecipes: async function (req, res, next) {
         try {
             const recipes = await Recipe.deleteMany({});
             res.status(200).json(recipes);
-
-        } catch (error) {
-            next(error);
-        }
-    },
-
-    deleteRecipe: async function (req, res, next) {
-        try {
-            const recipe = await Recipe.findByIdAndDelete(req.params.recipeId);
-
-            if (recipe === null) next();
-            else res.status(200).json(recipe);
 
         } catch (error) {
             next(error);
