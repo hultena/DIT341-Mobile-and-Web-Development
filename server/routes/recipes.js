bf0a748ad4cebf7f6d0fa9756d4ce4267722d1c6
Alexander Hultén <gusalexhu@student.gu.se>
2020-09-15 18:00:31 +0200

    Add parsing to recipes endpoints, fix #35

--- server/routes/recipes.js
+++ server/routes/recipes.js
@@ -1,10 +1,11 @@
 const express = require('express');
 const recipes = require('../controllers/recipes');
+const queryString = require('../utilities/query-parser');
 
 const router = express.Router();
 
 router.route('/')
-    .get(recipes.getAllRecipes)
+    .get(queryString.query(), recipes.getAllRecipes)
     .delete(recipes.deleteAllRecipes);
 
 module.exports = router;
