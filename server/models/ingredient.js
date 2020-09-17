1d743a8335c5c989154501ec6a7f38fbb62dd99a
Alexander Hultén <gusalexhu@student.gu.se>
2020-09-17 13:46:41 +0200

    Add cascade deletion, fix #39

--- server/models/ingredient.js
+++ server/models/ingredient.js
@@ -1,4 +1,6 @@
 const mongoose = require('mongoose');
+const ShoppingList = require('./shoppinglist');
+const Recipe = require('./recipe');
 const Schema = mongoose.Schema;
 
 const ingredientSchema = new Schema({
@@ -6,6 +8,23 @@ const ingredientSchema = new Schema({
     name:{type: String, required: true, unique: true},
     foodType:{type: String},
     description:{type: String, maxlength: 140}
+});
+
+ingredientSchema.pre('deleteMany', async function(){
+    await ShoppingList.updateMany({}, {$set: {ingredients: []}});
+    await Recipe.updateMany({}, {$set: {ingredients: []}});
+});
+
+ingredientSchema.pre('findOneAndDelete', async function() {
+    const ingredientId = this.getQuery()._id;
+    await ShoppingList.updateMany(
+        {ingredients: ingredientId},
+        {$pull: {ingredients: ingredientId
+        }});
+    await Recipe.updateMany(
+        {ingredients: ingredientId},
+        {$pull: {ingredients: ingredientId
+        }});
 });
 
 module.exports = mongoose.model('ingredient', ingredientSchema);
