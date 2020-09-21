1c4179f66b49695810c0b30d1c9224166e3837ac
livm <livm@student.chalmers.se>
2020-09-21 16:48:40 +0200

    Routes to new views #56

--- client/src/router.js
+++ client/src/router.js
@@ -2,6 +2,7 @@ import Vue from 'vue'
 import Router from 'vue-router'
 import Home from './views/Home.vue'
 import MyProfile from './views/MyProfile.vue'
+import About from './views/About.vue'
 
 Vue.use(Router)
 
@@ -18,6 +19,11 @@ export default new Router({
       path: '/my-profile',
       name: 'myProfile',
       component: MyProfile
+    },
+    {
+      path: '/about',
+      name: 'about',
+      component: About
     }
   ]
 })
