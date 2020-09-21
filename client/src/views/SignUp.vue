ef1d3160041a3db773693f6d85e2c58514dd4d17
livm <livm@student.chalmers.se>
2020-09-21 18:22:17 +0200

    Base of view for storing user sign up form, #56

---
+++ client/src/views/SignUp.vue
@@ -0,0 +1,17 @@
+<template>
+  <div class="signUpPage">
+    <h1>
+      This the user sign up page
+    </h1>
+  </div>
+</template>
+
+<script>
+export default {
+  name: 'signUp'
+}
+</script>
+
+<style scoped>
+.signUpPage {}
+</style>
