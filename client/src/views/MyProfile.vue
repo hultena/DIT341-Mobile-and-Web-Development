676952c73643b881585381a59ba01a39e5e449f5
livm <livm@student.chalmers.se>
2020-09-21 16:34:06 +0200

    Profile page, specific to current user, #56

---
+++ client/src/views/MyProfile.vue
@@ -0,0 +1,22 @@
+<template>
+  <div class='profilePage'>
+    <p>
+      This is a profile page
+    </p>
+  </div>
+</template>
+
+<script>
+export default {
+  name: 'myProfile',
+  data() {
+    return {}
+  },
+  methods: {}
+}
+
+</script>
+
+<style scoped>
+.profilePage {}
+</style>
