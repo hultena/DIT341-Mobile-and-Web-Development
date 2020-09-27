b957af6446720ed09632dc866ae4c546651991ff
Ella Alklid <ella.alklid@gmail.com>
2020-09-27 14:11:12 +0200

    Add footer component to app, #42

--- client/src/App.vue
+++ client/src/App.vue
@@ -13,20 +13,17 @@
     </header>
     <!-- Render the content of the current page view -->
     <router-view/>
-    <footer>
-      <router-link to='/'>
-        <img src="./assets/YummyLogo.svg">
-      </router-link>
-      <router-link to='/about'>
-        About
-      </router-link>
-      <router-link to='/faq'>
-        FAQ
-      </router-link>
-    </footer>
+    <Footer/>
   </div>
 </template>
-
+<script>
+import Footer from './components/Footer.vue'
+export default {
+  components: {
+    Footer
+  }
+}
+</script>
 <style>
 #app {
   font-family: 'Avenir', Helvetica, Arial, sans-serif;
