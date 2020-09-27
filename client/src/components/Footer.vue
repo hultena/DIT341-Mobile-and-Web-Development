fb6173bec78f06ba024d96672d7ea0afa9da0afc
Ella Alklid <ella.alklid@gmail.com>
2020-09-27 14:08:49 +0200

    Implement footer component, #42

--- client/src/components/Footer.vue
+++ client/src/components/Footer.vue
@@ -1,12 +1,27 @@
 <template>
-    <div>
-    </div>
+    <footer class='footer'>
+        <img src="../assets/YummyLogo.svg" alt="Logo" class="logo"/>
+        <b-nav align="center">
+            <b-nav-item to="/">Home</b-nav-item>
+            <b-nav-item to="/about">About Us</b-nav-item>
+            <b-nav-item to="/faq">FAQ</b-nav-item>
+            <b-nav-item href='#top'>Go to top</b-nav-item>
+        </b-nav>
+    </footer>
 </template>
 
 <script>
 export default {
+  name: 'footer',
+  data() {
+    return {}
+  },
+  methods: {}
 }
 </script>
 
 <style scoped>
+.footer {
+}
+.logo {}
 </style>
