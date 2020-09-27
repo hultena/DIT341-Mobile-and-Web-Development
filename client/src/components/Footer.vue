345d857bc78587864c3ba31bc0b448f89918b688
Ella Alklid <ella.alklid@gmail.com>
2020-09-27 14:18:39 +0200

    Add link to repo in footer, #42

--- client/src/components/Footer.vue
+++ client/src/components/Footer.vue
@@ -1,10 +1,12 @@
 <template>
     <footer class='footer'>
-        <img src="../assets/YummyLogo.svg" alt="Logo" class="logo"/>
         <b-nav align="center">
-            <b-nav-item to="/">Home</b-nav-item>
+            <b-nav-item to="/">
+                <img src="../assets/YummyLogo.svg" class="logo"/>
+            </b-nav-item>
             <b-nav-item to="/about">About Us</b-nav-item>
             <b-nav-item to="/faq">FAQ</b-nav-item>
+            <b-nav-item href='https://git.chalmers.se/courses/dit341/2020/group-01-web' target='_blank'>GitLab</b-nav-item>
             <b-nav-item href='#top'>Go to top</b-nav-item>
         </b-nav>
     </footer>
