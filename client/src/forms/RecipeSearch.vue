f80ec6ee8045331b6d8293334a4a51812441ce4b
livm <livm@student.chalmers.se>
2020-09-28 12:18:08 +0200

    Beginning of search form for recipes #40

---
+++ client/src/forms/RecipeSearch.vue
@@ -0,0 +1,32 @@
+<template>
+  <b-form-group>
+    <b-input-group>
+
+      <b-form-input
+        type='text'
+      />
+
+      <b-input-group-append>
+        <b-button>
+          Filter
+        </b-button>
+        <b-button
+          variant='primary'
+        >
+          <b-icon-search />
+        </b-button>
+      </b-input-group-append>
+
+    </b-input-group>
+  </b-form-group>
+</template>
+
+<script>
+export default {
+  name: 'RecipeSearch'
+}
+</script>
+
+<style scoped>
+
+</style>
