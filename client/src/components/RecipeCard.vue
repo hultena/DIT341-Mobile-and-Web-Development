50d512e52d18cf613699d269002fe9b7fdd7d47b
livm <livm@student.chalmers.se>
2020-09-28 17:12:43 +0200

    Props to take card data from parent #43 #40

--- client/src/components/RecipeCard.vue
+++ client/src/components/RecipeCard.vue
@@ -1,13 +1,16 @@
 <template>
   <b-card
-    title='Recipe name'
-    img-src='https://images.unsplash.com/photo-1566217224819-e093a5a0798e?ixlib=rb-1.2.1&ixid=eyJhcHBfaWQiOjEyMDd9&auto=format&fit=crop&w=750&q=80'
+    :title='title'
+    :img-src='imageSrc'
     img-top
     tag='recipe'
   >
     <b-card-text>
       <p>
-        Recipe description
+        <b-icon-heart /> {{ likes }}
+      </p>
+      <p>
+        {{ description | snippet }}
       </p>
     </b-card-text>
   </b-card>
@@ -17,7 +20,12 @@
 export default {
   name: 'RecipeCard',
 
-  props: {}
+  props: {
+    imageSrc: String,
+    title: String,
+    likes: Number,
+    description: String
+  }
 }
 </script>
 
--- client/src/components/RecipeSearch.vue
+++ client/src/components/RecipeSearch.vue
@@ -31,7 +31,12 @@
     </b-collapse>
 
     <div>
-      <RecipeCard />
+      <RecipeCard
+        :image-src='cardImage'
+        :title='cardTitle'
+        :likes='cardLikes'
+        :description='cardDescription'
+      />
     </div>
 
   </b-form-group>
@@ -47,8 +52,14 @@ export default {
   components: { RecipeCard },
 
   data() {
-    return {}
+    return {
+      cardImage: 'https://images.unsplash.com/photo-1566217224819-e093a5a0798e?ixlib=rb-1.2.1&ixid=eyJhcHBfaWQiOjEyMDd9&auto=format&fit=crop&w=750&q=80',
+      cardTitle: 'Recipe name',
+      cardLikes: 34,
+      cardDescription: 'Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum.'
+    }
   },
+
   methods: {
   }
 }
