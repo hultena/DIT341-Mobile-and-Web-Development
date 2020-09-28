9b66c48ebd6e3292a244deeb9e4b375e0c0ad016
Alexander Hultén <gusalexhu@student.gu.se>
2020-09-28 19:57:53 +0200

    Add shopping list view, fix #50 Add a basic shopping list card place holder, fix #64

---
+++ client/src/components/ShoppingListCard.vue
@@ -0,0 +1,53 @@
+<template>
+  <div>
+  <b-card>
+    <b-card-body>
+      <b-card-title>Card Title</b-card-title>
+      <b-card-sub-title class="mb-2">Card Sub Title</b-card-sub-title>
+      <b-card-text>
+        Some quick example text to build on the card title and make up the bulk of the card's
+        content.
+      </b-card-text>
+    </b-card-body>
+
+    <b-list-group flush>
+      <b-list-group-item>Cras justo odio</b-list-group-item>
+      <b-list-group-item>Dapibus ac facilisis in</b-list-group-item>
+      <b-list-group-item>Vestibulum at eros</b-list-group-item>
+    </b-list-group>
+
+    <b-card-body>
+      <a href="#" class="card-link">Card link</a>
+      <a href="#" class="card-link">Another link</a>
+    </b-card-body>
+
+    <b-card-footer>
+      <b-button>Edit</b-button>
+    </b-card-footer>
+  </b-card>
+  </div>
+</template>
+
+<script>
+import { mapActions, mapGetters } from 'vuex'
+export default {
+  name: 'ShoppingListCard',
+  data() {
+    return {
+      data: null
+    }
+  },
+  methods: {
+    ...mapActions(['getShoppingLists', 'postShoppingList', 'getIngredients'])
+  },
+  computed: {
+    ...mapGetters(['allShoppingLists', 'getLoggedIn'])
+  },
+  created() {
+  }
+}
+</script>
+
+<style scoped>
+
+</style>
---
+++ client/src/store/modules/shoppingLists.js
@@ -0,0 +1,68 @@
+import { Api } from '@/Api'
+
+const state = {
+  // put the state here
+  shoppingLists: [],
+  loggedIn: ''
+}
+
+const getters = {
+  // put state getters here
+  allShoppingLists: function (state) {
+    return state.shoppingLists
+  }
+}
+
+const mutations = {
+  // put methods that mutate the state here
+  setShoppingLists: function (state, shoppingLists) {
+    state.shoppingLists = shoppingLists
+  },
+  newShoppingList: function (state, shoppingLists) {
+    state.shoppingLists.push(shoppingLists)
+  },
+  deletedShoppingList: function (state, id) {
+    const idx = state.shoppingLists.findIndex(function (shoppingList) {
+      return shoppingList._id === id
+    })
+    state.shoppingLists.splice(idx, 1)
+  },
+  updateShoppingList: function (state, updatedShoppingList) {
+    const idx = state.shoppingLists.findIndex(function (shoppingList) {
+      return shoppingList._id === updatedShoppingList._id
+    })
+    state.shoppingLists.splice(idx, 1, updatedShoppingList)
+  }
+}
+
+const actions = {
+  // put method callers here
+  async getShoppingLists({ commit }, id) {
+    const res = await Api.get(`/users/${id}/shoppinglists/`)
+    commit('setShoppingLists', res.data)
+  },
+  async postShoppingList({ commit }, shoppingList) {
+    try {
+      console.log(shoppingList)
+      const res = await Api.post(`/users/${shoppingList.user}/shoppinglists/`, shoppingList)
+      commit('newShoppingList', res.data)
+    } catch (err) {
+      return err.response.data
+    }
+  },
+  async patchShoppingList({ commit }, shoppingList) {
+    const res = await Api.patch(`/users/${shoppingList.user}/shoppinglists/${shoppingList._id}`, shoppingList)
+    commit('setLoggedIn', res.data)
+  },
+  async deleteShoppingList({ commit }, shoppingList) {
+    await Api.delete(`/users/${shoppingList.user}/shoppinglists/${shoppingList._id}`)
+    commit('deletedUser', shoppingList._id)
+  }
+}
+
+export default {
+  state,
+  getters,
+  mutations,
+  actions
+}
