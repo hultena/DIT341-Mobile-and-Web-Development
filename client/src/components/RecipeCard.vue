386e0a97b3bd79168123633a4bed901c2063c880
livm <livm@student.chalmers.se>
2020-09-28 17:47:59 +0200

    Card assigned button role #43

--- client/src/components/RecipeCard.vue
+++ client/src/components/RecipeCard.vue
@@ -3,15 +3,21 @@
     :title='title'
     :img-src='imageSrc'
     img-top
-    tag='recipe'
+    role='button'
+    @click='handleClick'
   >
     <b-card-text>
-      <p>
-        <b-icon-heart /> {{ likes }}
-      </p>
+
+      <b-card-sub-title class='mb-3'>
+        <b-icon-heart /> {{ likes }},
+        created by
+        <b-link :href='profileLink'>{{ username }}</b-link>
+      </b-card-sub-title>
+
       <p>
         {{ description | snippet }}
       </p>
+
     </b-card-text>
   </b-card>
 </template>
@@ -24,11 +30,16 @@ export default {
     imageSrc: String,
     title: String,
     likes: Number,
+    profileLink: String,
+    username: String,
     description: String
+  },
+
+  methods: {
+    handleClick() {
+      // TODO: Take user to related recipe page on click.
+      console.log('This card has been clicked!')
+    }
   }
 }
 </script>
-
-<style scoped>
-
-</style>
