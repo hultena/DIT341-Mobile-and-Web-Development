de1a35322e026957d992c47fcc83979ff0cbb1f7
Alexander Hultén <gusalexhu@student.gu.se>
2020-09-29 20:26:46 +0200

    Remove unused place holder items from ShoppingListCard, fix #64

--- client/src/components/ShoppingListCard.vue
+++ client/src/components/ShoppingListCard.vue
@@ -1,15 +1,6 @@
 <template>
   <div>
   <b-card @click="setChosenCard">
-    <b-card-body>
-      <b-card-title>Card Title</b-card-title>
-      <b-card-sub-title class="mb-2">Card Sub Title</b-card-sub-title>
-      <b-card-text>
-        Some quick example text to build on the card title and make up the bulk of the card's
-        content.
-      </b-card-text>
-    </b-card-body>
-
     <b-list-group flush>
       <b-list-group-item v-for="ingredient in allIngredients" :key="ingredient._id">{{ ingredient.name }}</b-list-group-item>
     </b-list-group>
@@ -48,7 +39,6 @@ export default {
     edit() {
       // TODO: Enable editing of card
       this.state = !this.state
-      console.log(this.state)
     }
   },
   computed: {
