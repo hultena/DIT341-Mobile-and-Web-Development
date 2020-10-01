51b7baf152baabb1217d9bf6363d99262f9a99d8
livm <livm@student.chalmers.se>
2020-10-01 15:34:57 +0200

    Routes added to reach recipe form #45

---
+++ client/src/views/CreateRecipePage.vue
@@ -0,0 +1,18 @@
+<template>
+ <div class='create-recipe-page'>
+   <CreateRecipeForm />
+ </div>
+</template>
+
+<script>
+import CreateRecipeForm from '@/forms/CreateRecipeForm/CreateRecipeForm'
+
+export default {
+  name: 'CreateRecipePage',
+  components: { CreateRecipeForm }
+}
+</script>
+
+<style scoped>
+
+</style>
