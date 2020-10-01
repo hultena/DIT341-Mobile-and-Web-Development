9075dfc962c4a2329c64b010be569180468858cc
Alexander Hultén <gusalexhu@student.gu.se>
2020-10-01 14:27:10 +0200

    Some refactoring and removal of unnecessary created calls

--- client/src/views/MyProfile.vue
+++ client/src/views/MyProfile.vue
@@ -3,29 +3,42 @@
     <section>
       <h1>
         Welcome {{this.loggedInUser.username}}
-        <router-link to="my-settings">
-        <b-button>Settings</b-button>
-        </router-link>
-        <b-button
-          v-if="currentView==='ingredients'&&!addIngredientState"
-          @click="addIngredientState=changeState(addIngredientState)">
-          Add Ingredients</b-button>
-        <b-button
-          v-if="currentView==='ingredients'&&addIngredientState"
-          @click="addIngredientState=changeState(addIngredientState)">
-          Finish</b-button>
       </h1>
       <h2>
         {{this.loggedInUser.email}}
       </h2>
+      <h3>
+        <b-button
+          v-if="this.currentView==='ingredients'&&!addIngredientState"
+          @click="addIngredientState=changeState(addIngredientState)">
+          Add Ingredients
+        </b-button>
+        <b-button
+          v-if="this.currentView==='ingredients'&&addIngredientState"
+          @click="addIngredientState=changeState(addIngredientState)">
+          Finish
+        </b-button>
+        <b-button
+          v-if="this.currentView==='shopping'"
+          @click="addShoppingList">
+          Add Shopping List
+        </b-button>
+        <!-- Perhaps move this button to the navbar in the future? Putting it here for now -->
+          <b-button
+            v-if="this.currentView!=='settings'"
+            @click="changeView('settings')">
+            User Settings
+          </b-button>
+      </h3>
     </section>
     <section>
-      <add-ingredient-form v-if="currentView==='ingredients'&&addIngredientState"/>
+      <add-ingredient-form v-if="this.currentView==='ingredients'&&addIngredientState"/>
     </section>
     <section>
       <p v-if="this.currentView==='recipes'">Placeholder for my recipes</p>
       <my-ingredients v-if="this.currentView==='ingredients'"/>
       <my-shopping-lists v-if="this.currentView==='shopping'"/>
+      <my-settings v-if="this.currentView==='settings'"/>
       <p v-if="this.currentView==='favourites'">Placeholder favourite recipes</p>
     </section>
   </div>
@@ -36,22 +49,36 @@ import { mapGetters, mapActions } from 'vuex'
 import MyIngredients from '@/views/MyIngredients'
 import MyShoppingLists from '@/views/MyShoppingLists'
 import AddIngredientForm from '@/forms/AddIngredientForm'
+import MySettings from '@/views/MySettings'
 export default {
   name: 'myProfile',
-  components: { AddIngredientForm, MyShoppingLists, MyIngredients },
+  components: { MySettings, AddIngredientForm, MyShoppingLists, MyIngredients },
   data() {
     return {
-      addIngredientState: null
+      addIngredientState: null,
+      user: null
     }
   },
   methods: {
-    ...mapActions(['']),
+    ...mapActions(['getShoppingLists', 'getUserIngredients', 'changeView']),
     changeState(state) {
       return !state
+    },
+    addShoppingList() {
+      const shoppingList = {
+        user: this.user,
+        ingredients: []
+      }
+      this.postShoppingList(shoppingList)
     }
   },
   computed: {
     ...mapGetters(['loggedInUser', 'allShoppingLists', 'allUserIngredients', 'currentView'])
+  },
+  async created() {
+    this.user = this.loggedInUser._id
+    await this.getShoppingLists(this.user)
+    await this.getUserIngredients(this.user)
   }
 }
 
--- client/src/views/MyShoppingLists.vue
+++ client/src/views/MyShoppingLists.vue
@@ -1,6 +1,5 @@
 <template>
   <div>
-    <b-button @click="addShoppingList">Add Shopping List</b-button>
     <shopping-list-card
       v-for="shoppingList in allShoppingLists"
       :key="shoppingList._id"
@@ -21,21 +20,10 @@ export default {
     }
   },
   methods: {
-    ...mapActions(['getShoppingLists', 'postShoppingList', 'getIngredients']),
-    addShoppingList() {
-      const shoppingList = {
-        user: this.loggedInUser._id,
-        ingredients: []
-      }
-      this.postShoppingList(shoppingList)
-    }
+    ...mapActions(['getShoppingLists', 'postShoppingList', 'getIngredients'])
   },
   computed: {
-    ...mapGetters(['allShoppingLists', 'loggedInUser'])
-  },
-  async created() {
-    const user = this.loggedInUser._id
-    await this.getShoppingLists(user)
+    ...mapGetters(['allShoppingLists'])
   }
 }
 </script>
