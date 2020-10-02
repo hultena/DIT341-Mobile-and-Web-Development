657903771d418c93f67096ddf803cc981880bb1f
livm <livm@student.chalmers.se>
2020-10-02 19:26:35 +0200

    Recipe card loop removed #40

--- client/src/components/RecipeSearch.vue
+++ client/src/components/RecipeSearch.vue
@@ -34,79 +34,19 @@
       <div class='single-recipe'></div>
     </div>
 
-    <b-row>
-      <b-col
-        v-for='(recipe, index) in recipes'
-        :key='index'
-        class='my-2'
-      >
-        <RecipeCard
-          image-src='https://images.unsplash.com/photo-1588339724477-446bc8fac146?ixlib=rb-1.2.1&auto=format&fit=crop&w=634&q=80'
-          :title='recipe.name'
-          :likes='recipe.likes'
-          :username='recipe.user'
-          description='Hello world'
-          recipe-link='path'
-        />
-      </b-col>
-    </b-row>
-    <b-button
-      variant='primary'
-      @click='expandResults'
-      block
-      class='mt-2'
-    >
-      Show more results
-    </b-button>
   </b-form-group>
 </template>
 
 <script>
-import { Api } from '@/Api'
-import RecipeCard from '@/components/RecipeCard'
 
 export default {
   name: 'RecipeSearch',
 
-  components: { RecipeCard },
-
-  data() {
-    return {
-      recipes: [],
-      slice: 4,
-      currentLength: null,
-      previousLength: null
-    }
-  },
-
   methods: {
     // TODO: Get vuex to work and figure out why you get returned undefined.
-
-    getRecipes() {
-      Api.get('/recipes')
-        .then(response => {
-          this.recipes = response.data.slice(0, this.slice)
-        })
-        .catch(error => {
-          this.recipes = error
-        })
-    },
-
-    expandResults() {
-      this.slice += 4
-
-      Api.get('/recipes')
-        .then(response => {
-          this.recipes = response.data.slice(0, this.slice)
-        })
-        .catch(error => {
-          this.recipes = error
-        })
-    }
   },
 
   created() {
-    this.getRecipes()
   }
 }
 </script>
