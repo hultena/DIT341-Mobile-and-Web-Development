2f0362cc8b370b81c7a654b63318e5824406adbd
livm <livm@student.chalmers.se>
2020-10-05 17:04:35 +0200

    Deleted files removed from router #71

--- client/src/router.js
+++ client/src/router.js
@@ -9,8 +9,6 @@ import Faq from './views/Faq.vue'
 import MySettings from './views/MySettings'
 import MyShoppingLists from './views/MyShoppingLists'
 import MyRecipes from '@/views/MyRecipes'
-import CreateRecipePage from '@/views/CreateRecipePage'
-import EditRecipePage from '@/views/EditRecipePage'
 import RecipePage from '@/views/RecipePage'
 import MyRecipePage from '@/views/MyRecipePage'
 
@@ -69,16 +67,6 @@ export default new Router({
       path: '/my-recipe',
       name: 'MyRecipePage',
       component: MyRecipePage
-    },
-    {
-      path: '/create-recipe',
-      name: 'CreateRecipePage',
-      component: CreateRecipePage
-    },
-    {
-      path: '/edit-recipe',
-      name: 'EditRecipePage',
-      component: EditRecipePage
     },
     {
       path: '/recipe',
