3eab3e39ddffa5b3bbe60e91823cfcbd9984cd9e
livm <livm@student.chalmers.se>
2020-10-05 20:56:21 +0200

    Includes way of updating recipe image #33

--- client/src/views/MyRecipePage.vue
+++ client/src/views/MyRecipePage.vue
@@ -1,57 +1,57 @@
 <template>
-  <b-container>
-    <h1>
-      {{ recipe.name }}
-    </h1>
-    <b-row>
-      <b-col>
-        <edit-recipe-name-form :recipe='this.recipe' />
-      </b-col>
-    </b-row>
-    <b-row>
-      <b-col>
-        <edit-recipe-category-form :recipe='this.recipe' />
-      </b-col>
-    </b-row>
-    <b-row>
-      <b-col>
-        <edit-recipe-description-form :recipe='this.recipe' />
-      </b-col>
-    </b-row>
-    <b-row>
-      <b-col>
-        <ad-space />
-      </b-col>
-    </b-row>
-    <b-row>
-      <b-col>
-        <edit-recipe-dietary-restrictions-form :recipe='this.recipe' />
-      </b-col>
-      <b-col>
-        <edit-recipe-allergies-form :recipe='this.recipe' />
-      </b-col>
-    </b-row>
-    <b-row>
-      <b-col>
-        <edit-recipe-instructions :recipe='this.recipe' />
-      </b-col>
-    </b-row>
-    <b-row>
-      <b-col>
-        <b-card
-          title='Danger zone'
-        >
-          <b-button
-            variant='danger'
-            block
-            @click='deleteThisRecipe'
+  <div>
+    <edit-recipe-image :recipe='this.recipe' />
+    <b-container>
+      <b-row>
+        <b-col>
+          <edit-recipe-name-form :recipe='this.recipe' />
+        </b-col>
+      </b-row>
+      <b-row>
+        <b-col>
+          <edit-recipe-category-form :recipe='this.recipe' />
+        </b-col>
+      </b-row>
+      <b-row>
+        <b-col>
+          <edit-recipe-description-form :recipe='this.recipe' />
+        </b-col>
+      </b-row>
+      <b-row>
+        <b-col>
+          <ad-space />
+        </b-col>
+      </b-row>
+      <b-row>
+        <b-col>
+          <edit-recipe-dietary-restrictions-form :recipe='this.recipe' />
+        </b-col>
+        <b-col>
+          <edit-recipe-allergies-form :recipe='this.recipe' />
+        </b-col>
+      </b-row>
+      <b-row>
+        <b-col>
+          <edit-recipe-instructions :recipe='this.recipe' />
+        </b-col>
+      </b-row>
+      <b-row>
+        <b-col>
+          <b-card
+            title='Danger zone'
           >
-            Delete recipe
-          </b-button>
-        </b-card>
-      </b-col>
-    </b-row>
-  </b-container>
+            <b-button
+              variant='danger'
+              block
+              @click='deleteThisRecipe'
+            >
+              Delete recipe
+            </b-button>
+          </b-card>
+        </b-col>
+      </b-row>
+    </b-container>
+  </div>
 </template>
 
 <script>
@@ -63,11 +63,13 @@ import EditRecipeCategoryForm from '@/forms/recipes/EditRecipeCategory'
 import EditRecipeDietaryRestrictionsForm from '@/forms/recipes/EditRecipeDietaryRestrictionsForm'
 import EditRecipeAllergiesForm from '@/forms/recipes/EditRecipeAllergiesForm'
 import EditRecipeInstructions from '@/forms/recipes/EditRecipeInstructions'
+import EditRecipeImage from '@/forms/recipes/EditRecipeImage'
 
 export default {
   name: 'MyRecipePage',
 
   components: {
+    EditRecipeImage,
     EditRecipeInstructions,
     EditRecipeAllergiesForm,
     EditRecipeDietaryRestrictionsForm,
