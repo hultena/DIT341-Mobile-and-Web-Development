595f5b22e5da4f6b6c826ec5d28fad9ec50322a4
livm <livm@student.chalmers.se>
2020-10-05 18:02:44 +0200

    Forms to change name and description included #71

--- client/src/views/MyRecipePage.vue
+++ client/src/views/MyRecipePage.vue
@@ -3,14 +3,29 @@
     <h1>
       {{ recipe.name }}
     </h1>
+    <b-row>
+      <b-col>
+        <edit-recipe-name-form :recipe='this.recipe' />
+      </b-col>
+    </b-row>
+    <b-row>
+      <b-col>
+        <edit-recipe-description-form :recipe='this.recipe' />
+      </b-col>
+      <b-col></b-col>
+    </b-row>
   </b-container>
 </template>
 
 <script>
 import { mapGetters } from 'vuex'
+import EditRecipeNameForm from '@/forms/recipes/EditRecipeNameForm'
+import EditRecipeDescriptionForm from '@/forms/recipes/EditRecipeDescriptionForm'
 
 export default {
   name: 'MyRecipePage',
+
+  components: { EditRecipeDescriptionForm, EditRecipeNameForm },
 
   computed: { ...mapGetters(['oneRecipe']) },
 
