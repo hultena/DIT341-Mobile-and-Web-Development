1231d0331ace1a810d71754743010025b14e2cc9
livm <livm@student.chalmers.se>
2020-10-05 16:55:56 +0200

    Page for storing patch forms for the selected user recipe #71

---
+++ client/src/views/MyRecipePage.vue
@@ -0,0 +1,17 @@
+<template>
+  <b-container>
+    <h1>
+      My recipe page
+    </h1>
+  </b-container>
+</template>
+
+<script>
+export default {
+  name: 'MyRecipePage'
+}
+</script>
+
+<style scoped>
+
+</style>
