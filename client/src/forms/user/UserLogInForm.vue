8f0ec3f2b5ea007e53d958ea1c2f68b10beff986
livm <livm@student.chalmers.se>
2020-10-06 18:58:18 +0200

    Columns added to make the form wider in desktop view #81

--- client/src/forms/user/UserLogInForm.vue
+++ client/src/forms/user/UserLogInForm.vue
@@ -1,57 +1,61 @@
 <template>
   <validation-observer ref='observer' v-slot='{ handleSubmit }'>
     <b-form @submit.stop.prevent='handleSubmit(onSubmit)'>
+      <b-row>
+        <b-col>
+          <validation-provider
+            name='username'
+            :rules='{ required: true}'
+            v-slot='validationContext'
+          >
+            <b-form-group
+              label='Username'
+              label-for='username-input'
+            >
+              <b-input-group class='min-width-check'>
+                <b-input-group-prepend is-text>
+                  <b-icon-person />
+                </b-input-group-prepend>
+                <b-form-input
+                  v-model='form.username'
+                  :state='getValidationState(validationContext)'
+                  placeholder='Username'
+                  id='username-input'
+                />
+              </b-input-group>
 
-      <validation-provider
-        name='username'
-        :rules='{ required: true}'
-        v-slot='validationContext'
-      >
-        <b-form-group
-          label='Username'
-          label-for='username-input'
-        >
-          <b-input-group>
-            <b-input-group-prepend is-text>
-              <b-icon-person />
-            </b-input-group-prepend>
-            <b-form-input
-              v-model='form.username'
-              :state='getValidationState(validationContext)'
-              placeholder='Username'
-              id='username-input'
-            />
-          </b-input-group>
-
-          <b-form-invalid-feedback>{{ validationContext.errors[0] }}</b-form-invalid-feedback>
-        </b-form-group>
-      </validation-provider>
-
-      <validation-provider
-        name='password'
-        :rules='{ required: true}'
-        v-slot='validationContext'
-      >
-        <b-form-group
-          label='Password'
-          label-for='password-input'
-          class='mb-5'
-        >
-          <b-input-group>
-            <b-input-group-prepend is-text>
-              <b-icon-shield-lock />
-            </b-input-group-prepend>
-            <b-form-input
-              v-model='form.password'
-              :state='getValidationState(validationContext)'
-              type='password'
-              placeholder='Password'
+              <b-form-invalid-feedback>{{ validationContext.errors[0] }}</b-form-invalid-feedback>
+            </b-form-group>
+          </validation-provider>
+        </b-col>
+        <b-col>
+          <validation-provider
+            name='password'
+            :rules='{ required: true}'
+            v-slot='validationContext'
+          >
+            <b-form-group
+              label='Password'
               label-for='password-input'
-            />
-          </b-input-group>
-          <b-form-invalid-feedback>{{ validationContext.errors[0] }}</b-form-invalid-feedback>
-        </b-form-group>
-      </validation-provider>
+              class='mb-5'
+            >
+              <b-input-group class='min-width-check'>
+                <b-input-group-prepend is-text>
+                  <b-icon-shield-lock />
+                </b-input-group-prepend>
+                <b-form-input
+                  v-model='form.password'
+                  :state='getValidationState(validationContext)'
+                  type='password'
+                  placeholder='Password'
+                  label-for='password-input'
+                />
+              </b-input-group>
+              <b-form-invalid-feedback>{{ validationContext.errors[0] }}</b-form-invalid-feedback>
+            </b-form-group>
+          </validation-provider>
+        </b-col>
+      </b-row>
 
       <b-button
         type='submit'
@@ -103,5 +107,5 @@ export default {
 </script>
 
 <style scoped>
-
+.min-width-check { min-width: 200px; }
 </style>
