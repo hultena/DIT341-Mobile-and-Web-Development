959ff4ebeccc23dec7110a85b942d21817eebc30
livm <livm@student.chalmers.se>
2020-10-06 14:59:35 +0200

    Sections and base for grid + tiny search bar for question topic #81

--- client/src/views/Faq.vue
+++ client/src/views/Faq.vue
@@ -1,14 +1,52 @@
 <template>
-  <div class='faqPage'>
+  <b-container class='mt-5 faq-page'>
+
     <section>
-      <h1>
-        FAQ page heading
-      </h1>
-      <h2>
-        FAQ page subheading
-      </h2>
+
+      <b-row class='mt-5 icon-container'>
+        <b-col>
+          <b-icon-patch-question />
+        </b-col>
+      </b-row>
+
+      <b-row>
+        <b-col>
+          <h1>
+            Need help?
+          </h1>
+          <h2>
+            Have a read through our frequently asked questions
+          </h2>
+        </b-col>
+      </b-row>
+
     </section>
-  </div>
+
+    <section>
+      <b-row class='my-5'>
+        <b-col>
+          <h3>
+            Search for a topic
+          </h3>
+          <b-input-group>
+            <b-form-input
+              placeholder='Type your topic here ...'
+            />
+            <b-input-group-append>
+              <b-button
+                variant='outline-primary'
+              >
+                <b-icon-search />
+              </b-button>
+            </b-input-group-append>
+          </b-input-group>
+        </b-col>
+      </b-row>
+    </section>
+
+    <section></section>
+
+  </b-container>
 </template>
 
 <script>
@@ -23,5 +61,10 @@ export default {
 </script>
 
 <style scoped>
-.faqPage {}
+.faq-page {}
+.icon-container {
+  min-width: 500px;
+  font-size: 8rem;
+  text-align: center;
+}
 </style>
