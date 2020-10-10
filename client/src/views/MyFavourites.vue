c9ed167cd85dd5af825985ccc0a92503da6d7891
Alexander Hultén <gusalexhu@student.gu.se>
2020-10-10 19:25:39 +0200

    Add MyFavourites view, fix #89

---
+++ client/src/views/MyFavourites.vue
@@ -0,0 +1,30 @@
+<template>
+  <b-container>
+    <b-row>
+      <b-col
+        v-for='recipe in loggedInUser.favourites'
+        :key='recipe._id'>
+        <recipe-card :recipe='recipe' />
+      </b-col>
+    </b-row>
+  </b-container>
+</template>
+
+<script>
+import { mapGetters } from 'vuex'
+import RecipeCard from '@/components/RecipeCard'
+export default {
+  name: 'MyFavourites',
+  components: { RecipeCard },
+  data() {
+    return {
+    }
+  },
+  computed: {
+    ...mapGetters(['loggedInUser'])
+  }
+}
+</script>
+
+<style scoped>
+</style>
