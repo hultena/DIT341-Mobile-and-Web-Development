ceb2a8b83ca4cc6a3c72e5451ff89e3a009533d4
livm <livm@student.chalmers.se>
2020-10-19 18:31:14 +0200

    Profile hero turned into component #91

--- client/src/views/Profile.vue
+++ client/src/views/Profile.vue
@@ -1,42 +1,30 @@
 <template>
-  <b-container class='profile-page'>
+  <div class='profile-page'>
 
-    <b-row class='profile-header'>
-      <b-col class='avatar'>
-        <b-img
-          src='https://images.unsplash.com/photo-1494790108377-be9c29b29330?ixlib=rb-1.2.1&ixid=eyJhcHBfaWQiOjEyMDd9&auto=format&fit=crop&w=934&q=80'
-          alt='User avatar'
-          rounded='circle'
-        />
-      </b-col>
-      <b-col>
+    <profile-hero :user='this.user' />
 
-        <h1>{{ user.username }}</h1>
-
-      </b-col>
-    </b-row>
-
-  </b-container>
+  </div>
 </template>
 
 <script>
 import { mapGetters } from 'vuex'
+import ProfileHero from '@/components/ProfileHero'
 
 export default {
   name: 'profile',
-
+  components: { ProfileHero },
   computed: {
     ...mapGetters(['selectedUser'])
   },
 
   created() {
     this.user = this.selectedUser
-    console.log(this.user._id)
   },
 
   data() {
     return {
-      user: {}
+      user: {},
+      recipes: []
     }
   }
 }
@@ -46,24 +34,5 @@ export default {
 .profile-page {
   padding-top: 100px;
   padding-bottom: 100px;
-}
-
-.avatar {
-  max-width: 240px !important;
-}
-
-.avatar > img {
-  height: 200px;
-  width: 200px;
-  object-fit: cover;
-  object-position: center;
-}
-
-@media only screen and (max-width: 417px) {
-  .profile-header {
-    text-align: center;
-    justify-content: center;
-  }
-  div.avatar { padding-bottom: 30px }
 }
 </style>
